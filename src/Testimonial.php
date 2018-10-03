97b848a02552b8853532fd8eb9d14ddd85a566a8
Ed Beeny <edbeeny@gmail.com>
2018-10-03 20:21:37 +0100

    Php 7.2 Fix

--- src/Testimonial.php
+++ src/Testimonial.php
@@ -1,13 +1,13 @@
 <?php      
 namespace Concrete\Package\HwSimpleTestimonials\Src;
 
-use Concrete\Core\Foundation\Object as Object;
+use Concrete\Core\Foundation\ConcreteObject as ConcreteObject;
 use Database;
 
 use Concrete\Package\HwSimpleTestimonials\Src\TestimonialArrayList;
 
 defined('C5_EXECUTE') or die(_("Access Denied."));
-class Testimonial extends Object
+class Testimonial extends ConcreteObject
 {
 	 public static function getByID($sID) {
         $db = Database::get();
